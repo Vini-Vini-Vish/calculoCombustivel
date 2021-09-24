235578ae81ef6acaaead03fd45e32d0e7d0becba
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-09-24 17:59:37 -0300

    Create conta.js

---
+++ src/conta.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
+
+export default function Valores() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
